81edcb2a397d14873e3d19347bb91bef4ebc8c08
Paul-blgrn <paul.blgrn@gmail.com>
2025-03-13 21:53:01 +0100

    🚧 Convert project to typescript

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <h2>Bienvenue sur la page d'accueil de l'application !</h2>
+    </div>
+  );
+};
+
+export default Home;
