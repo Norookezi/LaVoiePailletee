99b7a1187b961ce56a8ffcdd2ab8303569a9097e
Paul-blgrn <paul.blgrn@gmail.com>
2025-03-14 21:57:11 +0100

    ✨ Add footer and update Home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from '../components/Home';
+import Footer from '../components/Footer';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-        <Home />
+    <div className="flex flex-col min-h-screen">
+        <main className="flex-grow bg-[#ffc53e]">
+          <Home />
+        </main>
+        <Footer />
     </div>
   );
 };
