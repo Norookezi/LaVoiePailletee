ad0fc67334faa801d2597d283653f123fdca5876
Norookezi <norookezi@pandacrp.com>
2025-03-14 22:23:33 +0100

    feat: :sparkles: Created header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from '../components/Home';
+import Header from '../components/Header';
 
 const HomePage: React.FC = () => {
   return (
     <div>
+        <Header />
         <Home />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kony: ['"kony"', "sans-serif"],
+        // Add more custom font families as needed
+      },
+    },
   },
   plugins: [],
 }
