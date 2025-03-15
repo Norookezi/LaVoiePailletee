82ae776ddf7624746361323bf1201385a161e415
Paul-blgrn <paul.blgrn@gmail.com>
2025-03-15 18:16:37 +0100

    ✨ Feat: partenaires

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ImageLink from '../components/reusable-ui/ImageLink';
 
 const HomePage: React.FC = () => {
   return (
     <main className='flex flex-col h-full min-h-screen'>
         <Header />
+
+        {/* First Section */}
         <section className='flex-row-reverse items-center justify-between pt-16 mx-5 max-md:mb-10 md:flex'>
           <div className='relative md:mx-[5vw] flex-1 self-stretch'>
             <h2 className='absolute block px-5 pt-2 text-center text-white -translate-y-1/2 shadow-lg md:pt-4 md:px-8 md:-translate-x-1/2 md:top-1/2 left-3 bg-mediumseagreen rounded-3xl whitespace-break-spaces font-kony'>
@@ -31,7 +34,26 @@ const HomePage: React.FC = () => {
             </div>
           </div>
         </section>
-        <Footer />
+
+        {/* Partenaires Section */}
+        <section className='flex-1 bg-[#ef476f] py-10 text-white text-center mt-10 md:mt-16 flex flex-col justify-center items-center'>
+          <div className="flex flex-col md:flex-col md:items-center md:justify-between md:mx-10">
+            <h2 className='text-3xl md:text-5xl font-bold mb-8 md:mb-12 md:mr-10 whitespace-normal md:whitespace-nowrap break-words'>
+              Nos partenaires
+            </h2>
+            <div className='flex flex-wrap justify-center sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-6 px-6 flex-1 min-w-0'>
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/le_refuge_2.webp" imageAlt="partenaire 1" />
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/la_cybergrange.webp" imageAlt="partenaire 2" />
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/ai_licia.webp" imageAlt="partenaire 3" />
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/skillcamp.png" imageAlt="partenaire 4" />
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/vent_divin.webp" imageAlt="partenaire 5" />
+              <ImageLink imageSize={20} link="#" imageSource="images/partenaires/le_shadock.png" imageAlt="partenaire 6" />
+            </div>
+          </div>
+        </section>
+        <div className="mt-auto">
+          <Footer />
+        </div>
     </main>
   );
 };
