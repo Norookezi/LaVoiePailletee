46d3d871b21177074a6c3afd4f1aad9dcfe129e9
Paul-blgrn <paul.blgrn@gmail.com>
2025-03-21 05:13:34 +0100

    💄 Removed: Top Right Menu (temporary)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,11 @@ const Header: React.FC = () => {
         src='images/degrade.webp'
         alt=''
       />
-      <div className='flex items-center justify-end w-screen px-5 pt-1 md:absolute md:top-1/2 md:-translate-y-1/2'>
+      {/* <div className='flex items-center justify-end w-screen px-5 pt-1 md:absolute md:top-1/2 md:-translate-y-1/2'>
         <span className='h-fit px-1.5 py-0 text-xl text-white uppercase border-4 border-white rounded-lg font-kony'>
           menu
         </span>
-      </div>
+      </div> */}
       <h1 className='w-screen py-4 pb-16 md:py-14 px-8 text-[3em] leading-[.9em] tracking-tighter text-center text-white font-kony'>
         La voie Pailletée
       </h1>
