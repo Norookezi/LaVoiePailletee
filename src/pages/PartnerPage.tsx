bc4411f0317ee651fa8772f407156139f81da0d8
Norookezi <norookezi@pandacrp.com>
2025-04-03 23:30:52 +0200

    fix: :lipstick: Fixed partner image heigth

--- src/pages/PartnerPage.tsx
+++ src/pages/PartnerPage.tsx
@@ -40,7 +40,7 @@ function makePartenaires(partenaires: partnerType[]): JSX.Element[] {
                 <div className="flex-[2] md:flex md:group-even:flex-row-reverse justify-between w-auto h-full bg-white p-10 pb-0 md:p-2 shadow-lg shadow-gray-500/50 rounded-xl group-odd:text-right relative">
                     <div className="absolute top-0 flex-1 p-10 overflow-hidden md:flex-none md:relative md:p-0 max-sm:!left-1/2 max-sm:top-0 max-sm:-translate-x-1/2 translate-x-[33%] group-even:right-0 group-odd:-translate-x-[52%] -translate-y-1/3 h-52 w-52 lg:w-[15rem] lg:h-[15rem] xl:h-[20rem] xl:w-[20rem] 2xl:w-[25rem] 2xl:h-[25rem]  max-h-full lg:p-4 md:!translate-x-0 md:!translate-y-0">
                         <img
-                            className={`object-contain w-auto h-full p-3 shadow-lg md:shadow-none bg-crimson rounded-xl overflow-hidden aspect-square shadow-gray-500 ${partenaire.className}`}
+                            className={`object-contain w-auto h-full max-md:h-[7rem] p-3 shadow-lg md:shadow-none bg-crimson rounded-xl overflow-hidden aspect-square shadow-gray-500 ${partenaire.className}`}
                             src={partenaire.image}
                             alt={`Logo ${partenaire.name}`}
                         />
