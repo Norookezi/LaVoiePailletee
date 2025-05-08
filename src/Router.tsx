2c00dfafae53e087fd531cfdea3ac3d462bdb086
Norookezi <norookezi@pandacrp.com>
2025-05-08 14:10:50 +0200

    feat: :sparkles: Added analytics into router

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,18 +4,34 @@ import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
 import StreamersPage from './pages/StreamersPage';
 import PartnerPage from './pages/PartnerPage';
+import CookiePage from './pages/Cookies';
+
+import { CookieService } from './services/cookie.service';
+import { Analystic } from './services/analystic.service';
+
+const cookieService = new CookieService();
+const analyticsService = new Analystic();
 
 const App: React.FC = () => {
     const location = useLocation();
     
     useEffect(() => {
-        console.log(`Location changed: ${location}`);
-    }, [location]);
+        const hasAnalyticsConsent = cookieService.get('Analytics') === true;
+      
+        if (hasAnalyticsConsent && analyticsService.isInitNeeded) {
+            analyticsService.initGA();
+        }
+      
+        if (hasAnalyticsConsent) {
+            analyticsService.pageView(location);
+        }
+    }, [location, cookieService, analyticsService]);
     return (
         <Routes>
             <Route path="*" element={<ErrorPage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="/partenaires" element={<PartnerPage />} />
+            <Route path="/cookies" element={<CookiePage />} />
             <Route path="/streamers" element={<StreamersPage />} />
         </Routes>
     );
